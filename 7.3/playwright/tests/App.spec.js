564b22b49630536f95331c686066d96845887001
NikitaBez <bezmenov.nikita@gmail.com>
2023-05-06 17:23:02 +0900

    HW 7.3.2 finished

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -1,22 +1,23 @@
 const { test, expect } = require("@playwright/test");
+const user = require("./user.js");
 
-test("test", async ({ page }) => {
-  // Go to https://netology.ru/free/management#/
-  await page.goto("https://netology.ru/free/management#/");
+test("Successful authorization", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").fill(user.username);
+  await page.getByPlaceholder("Пароль").fill(user.password);
+  await page.getByTestId("login-submit-btn").click();
 
-  // Click a
-  await page.click("a");
-  await expect(page).toHaveURL("https://netology.ru/");
+  await expect(page).toHaveURL("https://netology.ru/profile");
+  await expect(
+    page.getByRole("heading", { name: "Мои курсы и профессии" })
+  ).toBeVisible();
+});
 
-  // Click text=Учиться бесплатно
-  await page.click("text=Учиться бесплатно");
-  await expect(page).toHaveURL("https://netology.ru/free");
+test("Unsuccessful authorization", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").fill("usermail@mail.ru");
+  await page.getByPlaceholder("Пароль").fill("userpass");
+  await page.getByTestId("login-submit-btn").click();
 
-  page.click("text=Бизнес и управление");
-
-  // Click text=Как перенести своё дело в онлайн
-  await page.click("text=Как перенести своё дело в онлайн");
-  await expect(page).toHaveURL(
-    "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
-  );
+  await expect(page.getByTestId("login-submit-btn")).toBeVisible();
 });
